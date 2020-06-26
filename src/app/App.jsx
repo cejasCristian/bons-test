45dd877d93484569de37399b4d1cf0948a760f8f
cejasCristian <cejascristianh@gmail.com>
2020-06-26 13:38:35 -0300

    Added react router

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,25 @@
 import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import LoginPage from './pages/LoginPage';
+import GameboardPage from './pages/GameboardPage';
 import ModalMessage from './components/ModalMessage';
 
-const App = () => <ModalMessage />;
+const App = () => (
+  <Router>
+    <Switch>
+      <Route path='/' exact>
+        <LoginPage />
+      </Route>
+      <Route path='/gameboard'>
+        <GameboardPage />
+      </Route>
+      <Route path='/modal'>
+        <ModalMessage />
+      </Route>
+    </Switch>
+  </Router>
+);
 
 export default App;
